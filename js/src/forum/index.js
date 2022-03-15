355e354dfe820cab6d734b8674df57500db11964
Marco Colia <info@justoverclock.it>
2022-03-15 19:27:32 +0100

    fix for deleted users

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,98 +1,90 @@
 import app from 'flarum/forum/app';
-import {extend} from 'flarum/common/extend';
+import { extend } from 'flarum/common/extend';
 import CommentPost from 'flarum/forum/components/CommentPost';
 
 app.initializers.add('justoverclock/user-statistics', () => {
   extend(CommentPost.prototype, 'headerItems', function (items) {
-
     const post = this.attrs.post;
     const user = this.attrs.post.user();
-    const likeReceived = user.data.attributes.likesReceived;
+    if (user) {
+      const likeReceived = user.data.attributes.likesReceived;
 
-    if (post.isHidden()) return;
+      if (post.isHidden()) return;
 
       if (likeReceived >= 10 && likeReceived <= 49) {
-        items.add(
-          'topLikeReceived',
-          <div className="Badge-like ten">10+ {app.translator.trans('justoverclock-user-statistics.forum.likes')}</div>
-        )
+        items.add('topLikeReceived', <div className="Badge-like ten">10+ {app.translator.trans('justoverclock-user-statistics.forum.likes')}</div>);
       }
 
       if (likeReceived >= 50 && likeReceived <= 99) {
         items.add(
           'topLikeReceived',
-          <div
-            className="Badge-like fifthy">50+ {app.translator.trans('justoverclock-user-statistics.forum.likes')}</div>
-        )
+          <div className="Badge-like fifthy">50+ {app.translator.trans('justoverclock-user-statistics.forum.likes')}</div>
+        );
       }
 
       if (likeReceived >= 100 && likeReceived <= 199) {
         items.add(
           'topLikeReceived',
-          <div
-            className="Badge-like hundred">100+ {app.translator.trans('justoverclock-user-statistics.forum.likes')}</div>
-        )
+          <div className="Badge-like hundred">100+ {app.translator.trans('justoverclock-user-statistics.forum.likes')}</div>
+        );
       }
 
       if (likeReceived >= 200 && likeReceived <= 299) {
         items.add(
           'topLikeReceived',
-          <div
-            className="Badge-like twohundred">200+ {app.translator.trans('justoverclock-user-statistics.forum.likes')}</div>
-        )
+          <div className="Badge-like twohundred">200+ {app.translator.trans('justoverclock-user-statistics.forum.likes')}</div>
+        );
       }
 
       if (likeReceived >= 300 && likeReceived <= 499) {
         items.add(
           'topLikeReceived',
-          <div
-            className="Badge-like threehundred">300+ {app.translator.trans('justoverclock-user-statistics.forum.likes')}</div>
-        )
+          <div className="Badge-like threehundred">300+ {app.translator.trans('justoverclock-user-statistics.forum.likes')}</div>
+        );
       }
 
       if (likeReceived >= 500) {
         items.add(
           'topLikeReceived',
-          <div
-            className="Badge-like fivehundred">500+ {app.translator.trans('justoverclock-user-statistics.forum.likes')}</div>
-        )
+          <div className="Badge-like fivehundred">500+ {app.translator.trans('justoverclock-user-statistics.forum.likes')}</div>
+        );
       }
-  })
+    }
+  });
   extend(CommentPost.prototype, 'view', function (vnode) {
     const post = this.attrs.post;
     const user = this.attrs.post.user();
-    const joinTime = user.joinTime().toString().split(' ').slice(1, 4).join(' ');
-    const totalDiscussions = user.discussionCount();
-    const totalPosts = user.commentCount();
-    const likeReceived = user.data.attributes.likesReceived;
+    if (user) {
+      const joinTime = user.joinTime().toString().split(' ').slice(1, 4).join(' ');
+      const totalDiscussions = user.discussionCount();
+      const totalPosts = user.commentCount();
 
-    if (post.isHidden()) return;
+      const likeReceived = user.data.attributes.likesReceived;
+
+      if (post.isHidden()) return;
 
       vnode.children.push(
         <div className="user-stats">
           <ul>
             <li>
-              <i className="far fa-clock userstats"
-                 title={app.translator.trans('justoverclock-user-statistics.forum.joinTime')}/>
+              <i className="far fa-clock userstats" title={app.translator.trans('justoverclock-user-statistics.forum.joinTime')} />
               {joinTime}
             </li>
             <li>
-              <i className="far fa-comment-dots userstats"
-                 title={app.translator.trans('justoverclock-user-statistics.forum.totalDiscussions')}/>
+              <i className="far fa-comment-dots userstats" title={app.translator.trans('justoverclock-user-statistics.forum.totalDiscussions')} />
               {totalDiscussions}
             </li>
             <li>
-              <i className="fas fa-pen-nib userstats"
-                 title={app.translator.trans('justoverclock-user-statistics.forum.totalPosts')}/>
+              <i className="fas fa-pen-nib userstats" title={app.translator.trans('justoverclock-user-statistics.forum.totalPosts')} />
               {totalPosts}
             </li>
             <li>
-              <i className="fas fa-thumbs-up userstats"
-                 title={app.translator.trans('justoverclock-user-statistics.forum.likeReceived')}/>
+              <i className="fas fa-thumbs-up userstats" title={app.translator.trans('justoverclock-user-statistics.forum.likeReceived')} />
               {likeReceived}
             </li>
           </ul>
         </div>
       );
+    }
   });
 });
